3b34e952097d3971481969a69243150990e098ac
Indomanegement <indomanegement@zohomail.in>
2026-09-25 13:54:14 +0530

    Include Android splash source folder

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -23,6 +23,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDir("../Branding/App-Splash-Screen")
+        }
+    }
 }
 
 dependencies {
